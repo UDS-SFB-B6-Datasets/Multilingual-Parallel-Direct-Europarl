7022827f5bc8cce07ee05dc713aa0d4064b7fed0
userkay <39383743+userkay@users.noreply.github.com>
2021-04-29 21:24:33 +0200

    Update parallel_from_xml.py
    Add date-based selection

--- scripts/parallel_from_xml.py
+++ scripts/parallel_from_xml.py
@@ -4,13 +4,14 @@
 import argparse
 import os
 from typing import Optional, Union
-
+from datetime import datetime
 from tqdm import tqdm
 
 from scripts.utils import get_paths, read_xml, extract_text, extract_originals
 
 
-def get_comparable(xml_dir, src_lang, parallel_langs:Optional[Union[list, str]]=None, native=None, direct=0):
+def get_comparable(xml_dir, src_lang, parallel_langs:Optional[Union[list, str]]=None,
+                   native=None, direct=0, start_date=None, end_date=None):
     path_dict = get_paths(xml_dir, src_lang, parallel_langs)
     extracts = []
     if isinstance(src_lang, str):
@@ -20,7 +21,8 @@ def get_comparable(xml_dir, src_lang, parallel_langs:Optional[Union[list, str]]=
         print(f"Extracting parallel text for {sl} originals")
         for filepath in tqdm(path_dict[sl], position=0, leave=True):
             xml_tree = read_xml(filepath)
-            extracted = extract_originals(xml_tree, sl, native, direct=direct)
+            extracted = extract_originals(xml_tree, sl, native, direct=direct,
+                                          start_date=start_date, end_date=end_date)
             # print(len(extracted))
 
             filename = os.path.basename(filepath)
@@ -35,7 +37,8 @@ def get_comparable(xml_dir, src_lang, parallel_langs:Optional[Union[list, str]]=
                     parallel_filepath = os.path.join(xml_dir, l, f"{parallel_filename}.{l.upper()}.xml")
                     if parallel_filepath in path_dict[l]:
                         parallel_xml_tree = read_xml(parallel_filepath)
-                        parallels = extract_text(parallel_xml_tree, l, sl, native=native, direct=direct)
+                        parallels = extract_text(parallel_xml_tree, l, sl, native=native, direct=direct,
+                                                 start_date=start_date, end_date=end_date)
 
                         for k in extracted.keys():
                             if k in parallels.keys():
@@ -61,8 +64,10 @@ def write_to_tsv(data, file, sep="\t", **kwargs):
             f.write(sep.join(f"{x}" for x in line.values())+"\n")
     print(f"{len(data)} data written successfully")
 
-def main(xml_dir, outfile, src_lang, parallel_langs, native, direct):
-    comparable = get_comparable(xml_dir, src_lang, parallel_langs, native, direct)
+def main(xml_dir, outfile, src_lang, parallel_langs, native,
+         direct, start_date, end_date):
+    comparable = get_comparable(xml_dir, src_lang, parallel_langs,
+                                native, direct, start_date, end_date)
     outdir = os.path.dirname(outfile)
     if not os.path.exists(outdir):
         os.makedirs(outdir)
@@ -84,6 +89,14 @@ if __name__ == "__main__":
                         help="pass [1] to extract only texts with direct translations, \
                         [2] for texts with translations not guaranteed to be direct\
                         [0] or leave this argument out to extract everything.")
+    parser.add_argument("-f", "--fromdate", required=False, default=None,
+                        type=lambda s: datetime.strptime(s, '%Y%m%d'),
+                        help="optional start date for time range from which to extract data.\
+                             Format as YYYYMMDD")
+    parser.add_argument("-t", "--todate", required=False, default=None,
+                        type=lambda s: datetime.strptime(s, '%Y%m%d'),
+                        help="optional end date for time range from which to extract data.\
+                             Format as YYYYMMDD")
 
     args = parser.parse_args()
     input_dir = args.input
@@ -93,9 +106,12 @@ if __name__ == "__main__":
     parallel_langs = args.parallels
     native_speaker = args.native
     direct = args.direct
-    main(input_dir, output_dir, src, parallel_langs, native_speaker, direct)
+    start_date = args.fromdate
+    end_date = args.todate
+    if start_date is not None:
+        start_date = start_date.date()
+    end_date = args.todate
+    if end_date is not None:
+        end_date = end_date.date()
 
-
-#TODO: Separate direct and pivot
-#TODO: Add backtranslations
-#TODO: Add machine translations
+    main(input_dir, output_dir, src, parallel_langs, native_speaker, direct, start_date, end_date)
