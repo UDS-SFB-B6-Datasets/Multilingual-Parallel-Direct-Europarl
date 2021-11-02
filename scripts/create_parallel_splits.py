3a9de3a612f24ab2527b3f28146f85f545765e85
userkay <39383743+userkay@users.noreply.github.com>
2021-11-02 14:36:18 +0100

    Revised splitting algorithm

--- scripts/create_parallel_splits.py
+++ scripts/create_parallel_splits.py
@@ -4,146 +4,130 @@
 import argparse
 import os
 from math import floor
-from typing import Optional, Union
+from typing import Union
 
 import numpy
-import pandas
-from tqdm import tqdm
-
-from utils import get_paths, read_xml, extract_text, extract_originals
+import pandas as pd
 
 
-def read_table_with_pandas(filepath, **kwargs):
-    df = pandas.read_table(filepath, **kwargs)
-    return df
-
-
-def read_corpus(filepath, parallel_langs:Union[list, str, None]=None,
-                native=None, remove_originals=False, **kwargs):
+def read_corpus(filepath, src_langs:Union[list, str, None]=None, 
+                parallel_langs:Union[list, str, None]=None, 
+                direct=None, native=None, sep="\t"):
+    
     if isinstance(parallel_langs, str):
         parallel_langs = [parallel_langs]
-
-    if parallel_langs is not None:
-        subset = ["iid", "src"]
-        if not remove_originals:
-            subset.append("original")
-        if native is not None:
-            subset.append("native_speaker")
-        subset.extend(parallel_langs)
-        df = read_table_with_pandas(filepath, usecols=subset, **kwargs)
-
-    else:
-        subset = []
-        if remove_originals:
-            subset.append("original")
-        if native is None:
-            subset.append("native_speaker")
-        df = read_table_with_pandas(filepath, **kwargs)
-        df = df.drop(subset, axis = 1)
-    return df
-
-
-def filter(df, src_langs:Optional[list]=None, native=None, direct=0):
-    if direct == 1:
-        df = df[df.iid.str[:4].astype(int) < 2004]
-    elif direct == 2:
-        df = df[df.iid.str[:4].astype(int) > 2003]
+        
+    if isinstance(src_langs, str):
+        src_langs = [src_langs]
+    
+    cols = ["iid", "src", "native_speaker", "original"]
+    
+    print("Reading data")
+    df = pd.read_table(filepath, sep="\t")
+    
+    if parallel_langs is None:
+        parallel_langs = list(set(df.columns.tolist()) - set(cols))
+        
+    if src_langs is None:
+        src_langs = df.src.values().tolist()
+        
+    columns = cols + parallel_langs
+    
+    df = df[columns]
+    df = df.dropna()
+    
+    df['direct'] = numpy.where(df.iid.str[:4].astype(int) < 2004, 1, 0)
+    if direct is not None:
+        print("Filtering translation type: direct or undefined")
+        df = df[df.direct == direct]
     if native is not None:
+        print("Applying (non) native speaker filter")
         df = df[df.native_speaker == native]
     if src_langs:
+        print("Extracting selected source languages and parallels")
         df = df[df.src.isin(src_langs)]
+#     df = df.drop(subset, axis = 1)
     return df
 
-def get_data(filepath, src_langs:Union[list, str, None]=None,
-             parallel_langs:Union[list, str, None]=None, native=None, direct=0, remove_originals=False, **kwargs):
-    if "usecols" in kwargs.keys():
-        subset = kwargs.pop("usecols")
 
-    df = read_corpus(filepath, parallel_langs, native, **kwargs)
-    df = filter(df, src_langs, native, direct)
-    return df.dropna()
+def balance_classes(df, train_ratio=0.7, dev_ratio=0.15, random_state=None):
+    import math
+    print("Creating train-dev-test splits")
+    src = df.src.value_counts()
+    num_src = src.min()
+    
+    train, dev, test = [], [], []
+    
+    test_ratio = 1 - (train_ratio + dev_ratio)
+    num_train = math.floor(train_ratio * num_src)
+    num_dev = math.floor(dev_ratio * num_src)
+    
+    for sl in src.index:
+        source_lang = df[df.src == sl].sample(num_src, random_state=random_state)
+        tr = source_lang.sample(num_train, random_state=random_state)
+        source_lang.drop(tr.index, inplace=True)
+        dv = source_lang.sample(num_dev, random_state=random_state)
+        source_lang.drop(dv.index, inplace=True)
+        train.append(tr)
+        dev.append(dv)
+        test.append(source_lang)
+    
+    print("Train-dev-test splits created")
+    return pd.concat(train), pd.concat(dev), pd.concat(test)
 
 
-def get_proportions(L, proportions:list=[0.7, 0.15]):
-    if sum(proportions) > 1:
-        raise Exception("Sum of train and dev ratios should be <= 1")
-    tr, dev = tuple(proportions)
-    train_size = floor(L * tr)
-    dev_size = floor(L * dev)
-    test_size = L - train_size - dev_size
-    return train_size, dev_size, test_size
-
-
-def split(df, n_train, n_dev, n_test):
-    train = df.sample(n_train)
-    df = df.drop(train.index)
-    dev = df.sample(n_dev)
-    test = df.drop(dev.index).sample(n_test)
-    return train, dev, test
-
-
-def main(infile, outdir, src_langs, parallel_langs, native, direct,
-         tsplit, dsplit, remove_originals, **kwargs):
-    df = get_data(infile, src_langs, parallel_langs, native, direct, remove_originals, **kwargs)
-
-    L = df.src.value_counts().min()
-    a, b, c = get_proportions(L, [tsplit, dsplit])
-    print(L, a, b, c)
-
-    train_splits, dev_splits, test_splits = {}, {}, {}
-    for k in src_langs:
-        train_splits[k], dev_splits[k], test_splits[k] = split(df[df.src == k].sample(L), a, b, c)
-
-    train_df = pandas.concat(train_splits.values())
-    dev_df = pandas.concat(dev_splits.values())
-    test_df = pandas.concat(test_splits.values())
-
-    print(test_df.src.value_counts())
-
-    outdir = os.path.dirname(outdir)
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-    train_df.to_csv(os.path.join(outdir, "parallel_train.tsv"), sep="\t", index=False)
-    dev_df.to_csv(os.path.join(outdir, "parallel_dev.tsv"), sep="\t", index=False)
-    test_df.to_csv(os.path.join(outdir, "parallel_test.tsv"), sep="\t", index=False)
-    print(f"Data split successfully")
+def main(infile, outfile_prefix, src_langs, parallel_langs, 
+         native=None, direct=None, sep="\t", train_ratio=0.7, 
+         dev_ratio=0.15, random_state=1):
+    
+    df = read_corpus(filepath=infile, src_langs=src_langs, 
+                     parallel_langs=parallel_langs, direct=direct, 
+                     native=native, sep=sep)
+    train_df, dev_df, test_df = balance_classes(df, train_ratio=train_ratio, 
+                                       dev_ratio=dev_ratio, random_state=random_state)
+    
+    print(f"Saving splits to {outfile_prefix}_*.tsv")
+    train_df.to_csv(os.path.join(f"{outfile_prefix}_train.tsv"), sep="\t", index=False)
+    dev_df.to_csv(os.path.join(f"{outfile_prefix}_dev.tsv"), sep="\t", index=False)
+    test_df.to_csv(os.path.join(f"{outfile_prefix}_test.tsv"), sep="\t", index=False)
+    print(f"Data saved successfully")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract text from xml files')
     parser.add_argument("-i", "--input", required=True, help="path to input data file")
-    parser.add_argument("-o", "--output", required=True, help="output directory")
-    # parser.add_argument("-l", "--lang", required=True, help="language of text to extract")
+    parser.add_argument("-o", "--output", required=True, help="output file path and name prefix")
     parser.add_argument("-s", "--src", required=False, nargs='*', help="source/original language of text")
     parser.add_argument("-p", "--parallels", required=False, nargs='*', help="parallel language(s) to extract")
     parser.add_argument("-n", "--native", required=False, type=int, choices=[0, 1],
                         help="pass [1] to extract text from native speakers only, \
                         [0] for text from non-native speakers only. \
                         Leave out this argument to extract everything.")
-    parser.add_argument("-d", "--direct", required=False, type=int, default=0, choices=[0, 1, 2],
+    parser.add_argument("-d", "--direct", required=False, type=int, choices=[0, 1],
                         help="pass [1] to extract only texts with direct translations, \
-                        [2] for texts with translations not guaranteed to be direct\
-                        [0] or leave this argument out to extract everything.")
-    parser.add_argument("-t", "--train_split", required=False, type=float, default=0.7,
+                        [0] for texts with translations not guaranteed to be direct.\
+                        Leave this argument out to extract everything.")
+    parser.add_argument("-t", "--train_ratio", required=False, type=float, default=0.7,
                         help="fraction of data for train split. Test size is calculated from train and dev splits.")
-    parser.add_argument("-v", "--dev_split", required=False, type=float, default=0.15,
+    parser.add_argument("-v", "--dev_ratio", required=False, type=float, default=0.15,
                         help="fraction of data for dev split. Test size is calculated from train and dev splits.")
-    parser.add_argument('-r', "--remove", action='store_true',
-                        help="if not specified, originals are kept for each translation")
+    parser.add_argument('-r', "--random_state", required=False, type=int, default=1,
+                        help="seed for randomly sampling examples"),
+    parser.add_argument("--sep", required=False, default="\t", help="delimiter used in input file")
 
     args = parser.parse_args()
     infile = args.input
-    outdir = args.output
-    # lang = args.lang
+    outfile_prefix = args.output
     src_langs = args.src
     parallel_langs = args.parallels
     native = args.native
     direct = args.direct
-    tsplit = args.train_split
-    dsplit = args.dev_split
-    remove_originals = args.remove
+    train_ratio = args.train_ratio
+    dev_ratio = args.dev_ratio
+    random_state = args.random_state
+    sep = args.sep
 
-    main(infile, outdir, src_langs, parallel_langs, native, direct,
-         tsplit, dsplit, remove_originals)
-
-#TODO: Create splits
+    main(infile=infile, outfile_prefix=outfile_prefix, 
+         src_langs=src_langs, parallel_langs=parallel_langs, 
+         native=native, direct=direct, sep=sep, train_ratio=train_ratio, 
+         dev_ratio=dev_ratio, random_state=random_state)
